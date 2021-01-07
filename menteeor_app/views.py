f918f9b7b707475116821e3f0a6a981384e74aaa
Oladapo Laniyan <48382887+sunams@users.noreply.github.com>
2021-01-06 21:56:31 -0600

    Views created
    Views (welcome and account) created
    App installed in project

--- menteeor_app/views.py
+++ menteeor_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def account(request):
+    return render(request, 'account.html')

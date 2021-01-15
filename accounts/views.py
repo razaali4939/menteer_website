8a66cb8c48f7b18520d6f4aa132fc76dd31ae6cd
Oladapo Laniyan <48382887+sunams@users.noreply.github.com>
2021-01-14 22:33:36 -0600

    views updated
    Accounts views updated

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 # Create your views here.
 def register(request):
-    return render(request, 'register.html')
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid:
+            form.save()
+            
+    context = {'form':form}
+    return render(request, 'register.html', context)
 
 def login(request):
     return render(request, 'login.html')
